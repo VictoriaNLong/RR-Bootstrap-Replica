7fa8a9b8d2a79c700ba01acef821fd486ba06fce
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-05-06 18:43:44 -0700

    done

--- src/components/StoreListing.jsx
+++ src/components/StoreListing.jsx
@@ -10,8 +10,12 @@ const StyledRow = styled(Row)`
 border-bottom: 1px solid #e8e9eb;
 display: flex;
 justify-content: space-evenly;
-
 `
+
+const InnerRow = styled(Row)`
+flex-direction: row;
+`
+
 const CircleDiv = styled.div`
 width: 60px
 height: 60px
@@ -24,35 +28,35 @@ export default function StoreListing() {
     return (
    <StyledContainer>
     <StyledRow>
-        <Row>
-            <Col xs='0.5'>
+        <InnerRow>
+            <Col>
                 <CircleDiv/>
             </Col>
             <Col>
             <h5 style={{fontWeight: 'bold'}}>Aldi</h5>
             <p>Delivery and Pickup</p>
             </Col>
-        </Row>
+        </InnerRow>
         <Col sm='1'></Col>
-        <Row>
-            <Col xs='0.5'>
+        <InnerRow>
+            <Col>
                 <CircleDiv/>
             </Col>
             <Col>
             <h5 style={{fontWeight: 'bold'}}>Scott's Veg</h5>
             <p>Delivery and Pickup</p>
             </Col>
-        </Row>
+        </InnerRow>
         <Col sm='1'></Col>
-        <Row>
-            <Col xs='0.5'>
+        <InnerRow>
+            <Col>
                 <CircleDiv/>
             </Col>
             <Col>
             <h5 style={{fontWeight: 'bold'}}>Kroger</h5>
             <p>Delivery and Pickup</p>
             </Col>
-        </Row>
+        </InnerRow>
     </StyledRow>
    </StyledContainer>
     )
