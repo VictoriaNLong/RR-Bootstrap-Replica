2e1e46cf941e3cd118f3aa7199266786df3a1917
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-05-06 18:43:26 -0700

    fin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import TopBar from './components/TopBar';
+import styled from 'styled-components';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import splashImage from './splash.webp'
+import StoreListing from './components/StoreListing';
+
+const Splash = styled.div`
+width: 99vw;
+height: 500px;
+background-repeat: no-repeat;
+background-size: cover;
+background-image: url(${splashImage});
+background-color: #deedd6;
+position: absolute;
+left: 0;
+top: 40px;
+right: 0;
+z-index: -1
+`
+
+const StyledStoreListing = styled(StoreListing)`
+position: relative;
+top: 600px
+`
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <TopBar />
+    <StyledStoreListing/>
+    <Splash/>
+    </>
   );
 }
 
